c797a802037e07e0a38452c82430c87d77827a12
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:47:58 -0800

    Código del ejercicio

--- Ejercicio-1.js
+++ Ejercicio-1.js
@@ -4,3 +4,10 @@
 //1.Solicite al usuario que ingrese un número cualquiera.
 //2.Calcule el doble de ese número.
 //3.Muestre el resultado en la consola.
+
+const prompt = require('prompt-sync')();
+
+let EnterNumber = parseFloat(prompt("Enter a number:"));
+let DoubleNumber = (EnterNumber * 2);
+
+console.log("The double of your number is:" , EnterNumber);
