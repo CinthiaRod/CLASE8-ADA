c5ab09ee7277bb26daae32bff812fe7c3e7adac7
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:45:39 -0800

    Descripción del ejercicio

--- Ejercicio-5.js
+++ Ejercicio-5.js
@@ -1,1 +1,5 @@
-Ejercicio
+//Ejercicio 5: Saludo personalizado
+//Enunciado:
+//Este ejercicio es ideal para familiarizarse con la manipulación de cadenas de texto y la función prompt(). Escribe un programa que:
+//1.Solicite al usuario que ingrese su nombre.
+//2.Muestre un saludo personalizado usando el nombre ingresado.
