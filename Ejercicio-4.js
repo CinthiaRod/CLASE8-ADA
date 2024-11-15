45ec2da9f83d123f915133f99010f8b771db458d
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:42:16 -0800

    Descripción del ejercicio

--- Ejercicio-4.js
+++ Ejercicio-4.js
@@ -1,1 +1,7 @@
-Ejercicio
+//Ejercicio 4: Determinar si un número es par o impar
+//Enunciado:
+//En este ejercicio, aprenderás a utilizar operadores aritméticos y condicionales. 
+//Debes escribir un programa que:
+//1.Solicite al usuario que ingrese un número entero.
+//2.Determine si el número es par o impar.
+//3.Muestre un mensaje indicando si el número es par o impar.
