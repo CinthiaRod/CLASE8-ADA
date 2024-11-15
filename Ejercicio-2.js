288fd96906f3add79a8e76201b1b2b7d66d008f5
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:38:52 -0800

    Descripción del ejercicio

--- Ejercicio-2.js
+++ Ejercicio-2.js
@@ -1,1 +1,7 @@
-Ejercicio
+//Ejercicio 2: Verificar si un número es positivo, negativo o cero
+//Enunciado:
+//En este ejercicio, practicarás el uso de condicionales (if, else). 
+//Escribe un programa en JavaScript que:
+//1.Pida al usuario que ingrese un número.
+//2.Verifique si el número es positivo, negativo o igual a cero.
+//3.Muestre un mensaje indicando cuál es el caso.
