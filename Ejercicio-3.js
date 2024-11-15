a6f6125729c6c36326578f29f96533747a9a085b
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:40:40 -0800

    Descripción del ejercicio

--- Ejercicio-3.js
+++ Ejercicio-3.js
@@ -1,1 +1,7 @@
-Ejercicio
+//Ejercicio 3: Suma de dos números ingresados por el usuario
+//Enunciado:
+//Este ejercicio te ayudará a practicar la entrada de datos y el uso de operadores aritméticos. 
+//Escribe un programa que:
+//1.Pida al usuario que ingrese dos números.
+//2.Sume ambos números.
+//3.Muestre el resultado de la suma.
