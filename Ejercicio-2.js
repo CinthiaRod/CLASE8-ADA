6d842a47c96372c40f40f7cb6ffb28a1f175b257
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 23:12:08 -0800

    Código del ejercicio

--- Ejercicio-2.js
+++ Ejercicio-2.js
@@ -5,3 +5,20 @@
 //1.Pida al usuario que ingrese un número.
 //2.Verifique si el número es positivo, negativo o igual a cero.
 //3.Muestre un mensaje indicando cuál es el caso.
+
+const prompt =require('prompt-sync')();
+
+let Number = parseFloat(prompt("Enter a number:"));
+let Message ='';
+
+if (Number > 0){
+    Message = 'Positive';
+}else if (Number < 0){
+    Message = 'Negative';
+}else if(Number == 0){
+    Message ='Zero';
+}else {
+    Message ='Invalid number'
+}
+
+console.log ('The number is:' , Message);
