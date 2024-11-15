50b44ecdaefe6b2315ed761abc1790ff693196b9
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 23:59:26 -0800

    Código del ejercicio

--- Ejercicio-4.js
+++ Ejercicio-4.js
@@ -5,3 +5,16 @@
 //1.Solicite al usuario que ingrese un número entero.
 //2.Determine si el número es par o impar.
 //3.Muestre un mensaje indicando si el número es par o impar.
+
+const prompt =require('prompt-sync')();
+
+let Number = parseFloat(prompt('Enter a number:'));
+let Answer ='';
+if (Number % 2 ===0){
+    Answer = 'Pair';
+}else if(Number % 2 !==0 && Number/Number===1){
+    Answer ='Odd';
+}else {
+    Answer ='Invalid number';
+}
+console.log ('The number is:' , Answer);
