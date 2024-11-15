d0e757ff175bf86b891eaa03d59526f9def79d04
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-14 22:37:18 -0800

    Descripción del ejercicio

--- Ejercicio-1.js
+++ Ejercicio-1.js
@@ -1,1 +1,6 @@
-Ejercicio
+//Ejercicio 1: Calcula el doble de un número
+//En este ejercicio, practicarás el uso de variables y operadores aritméticos. 
+//Debes escribir un programa en JavaScript que:
+//1.Solicite al usuario que ingrese un número cualquiera.
+//2.Calcule el doble de ese número.
+//3.Muestre el resultado en la consola.
